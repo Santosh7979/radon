61da70db14f889a6a554b898b4a478bfc93fe728
Santosh7979 <santoshmahtomails2439@gmail.com>
2022-06-13 10:02:43 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,21 +8,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://santosh7979:Santosh7979@cluster0.sthglu8.mongodb.net/authors1?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
-
 app.use('/', route);
-
+app.use(function (req, res, next) {
+    var currentDate =new Date()
+    var datetime = currentDate.getDate() +" " 
+                    +(currentDate.getMonth()+1) +" "
+                    currentDate.getYear() +" "
+                    currentDate.getHours() +" "
+                    currentDate.getMinutes() +" "
+                    currentDate.getSeconds();
+    let ip = req.ip
+    let url =req.originalUrl
+    console.log(`${currentDate} ${ip} ${url}`)                
+        next() 
+}
+)
+// module.exports.newMiddleware=newMiddleware 
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
