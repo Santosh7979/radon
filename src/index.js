37b057c096a90ea4cbbbdec13165d066c7709157
Santosh7979 <santoshmahtomails2439@gmail.com>
2022-06-17 19:25:28 +0530

    done

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://santosh7979:Santosh7979@cluster0.sthglu8.mongodb.net/Authorisation2", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
